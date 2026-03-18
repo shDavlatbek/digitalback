a41f4c25f508c48a20a4993828fa0cf716a917c9
shDavlatbek <sh.davlatbek006@gmail.com>
2026-03-18 16:14:49 +0500

    upd, minor changes

--- config/settings/develop.py
+++ config/settings/develop.py
@@ -13,4 +13,6 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
 CORS_EXPOSE_HEADERS = ['Content-Type', 'X-CSRFToken']
+
+IMGPROXY_BASE_URL = env.str('IMGPROXY_BASE_URL', 'http://localhost:6008')
 # CORS_ALLOW_HEADERS = list(default_headers) + ["School"]
